427ee97cf71f9e35e617e64484374dbd462ad6d1
ashhiew <ash.hiew@gmail.com>
2019-08-15 18:18:53 +1200

    Updates

---
+++ precache-manifest.ba312336fb6bdfbb327780e68b9900d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4be1d3487224f434b3140986227d8f5",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "92206a4b044de22c8d3e",
+    "url": "/monsters-rolodex/static/css/main.e0be6b07.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monsters-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "92206a4b044de22c8d3e",
+    "url": "/monsters-rolodex/static/js/main.fbca8ab7.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
